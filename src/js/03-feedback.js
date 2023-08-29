a49b1af3058b2edc3d16ec128c8d4946337e77aa
EugeniaO <eugenia.ovcharenko@gmail.com>
2023-08-29 23:21:50 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,45 @@
 import throttle from 'lodash.throttle';
 
-const refs = {
-    form: document.querySelector(".feedback-form"),
-            email: document.querySelector(".feedback-form input"),
+const form = document.querySelector(".feedback-form");
+        
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("input", throttle(onTextareaInput, 500));
 
-        textarea: document.querySelector(".feedback-form textarea")
-} 
-
-refs.form.addEventListener("submit", onFormSubmit);
-refs.form.addEventListener("input", throttle(onTextareaInput, 500));
 const formData = {};
 
 populateData();
 
-
-
 function onTextareaInput(evt) {
-    
     formData[evt.target.name] = evt.target.value;
-    // console.log(formData);
-
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-
 };
-
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    evt.currentTarget.reset();
-localStorage.removeItem("feedback-form-state");
+    const data = new FormData(form);
+
+    for (let pair of data.entries()) {
+        if (!pair[1]) {
+            alert("Please fill in all the fields!");
+            return;
+        }
+    }
+
+    localStorage.removeItem("feedback-form-state");
     console.log(formData);
- };
+    evt.currentTarget.reset();
+};
 
 
 function populateData() {
-    const savedData = localStorage.getItem("feedback-form-state",);
+    const savedData = localStorage.getItem("feedback-form-state");
+
     if (savedData) {
-            const parsedData = JSON.parse(savedData);
+        const parsedData = JSON.parse(savedData);
 
         Object.entries(parsedData).forEach(([name, value]) => {
-            console.log(name, value);
-            refs.form.elements[name].value = value;
-        }
-
-        );
+            form.elements[name].value = value;
+            formData[name] = value;
+        });
     }
  }
